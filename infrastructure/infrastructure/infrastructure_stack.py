83f4dbfe698bc9f24ca658d7226b62f500473ff9
Stephen Sailor <stephen.sailor@cleanslatetg.com>
2022-10-22 14:15:48 -0400

    Updating Jekyll Version command in CodeBuild

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -182,7 +182,7 @@ class InfrastructureStack(Stack):
                         "install":{
                             "commands": [
                                 "cd blog",
-                                "gem install jekyll bundler -v 4.2.2",
+                                "gem install bundler jekyll:4.2.2",
                                 "bundle install"
                             ]
                         },
